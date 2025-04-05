f2d0681c07a1f253b0677524800ae5b05f18f5e4
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-04-05 17:58:34 +0300

    Initial commit of Huffman Coding project

---
+++ src/main/java/mg/huffman/Main.java
@@ -0,0 +1,13 @@
+package mg.huffman;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Create a JFrame window
+        JFrame frame = new JFrame("Swing Application");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 200);
+        frame.setVisible(true);
+    }
+}
