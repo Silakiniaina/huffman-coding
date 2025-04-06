f465611f4f75e498dbd5822a91ff73fdc1920e8f
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-04-06 20:55:11 +0300

    feat : Add function main to test on HuffmanCoding.

--- src/main/java/mg/huffman/HuffmanCoding.java
+++ src/main/java/mg/huffman/HuffmanCoding.java
@@ -1,5 +1,34 @@
 package mg.huffman;
 
+import mg.huffman.io.HuffmanIO;
+
 public class HuffmanCoding {
     
+    /* -------------------------------------------------------------------------- */
+    /*                                    Test                                    */
+    /* -------------------------------------------------------------------------- */
+    public static void main(String[] args) {
+        try {
+            if (args.length < 3) {
+                System.out.println("Usage: java HuffmanCoding [compress] inputFile outputFile");
+                return;
+            }
+            
+            String mode = args[0];
+            String inputFile = args[1];
+            String outputFile = args[2];
+            
+            HuffmanIO huffmanIO = new HuffmanIO();
+            
+            if ("compress".equals(mode)) {
+                huffmanIO.compressFile(inputFile, outputFile);
+                System.out.println("File compressed successfully!");
+            }else {
+                System.out.println("Unrecognized mode. Use 'compress'");
+            }
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
