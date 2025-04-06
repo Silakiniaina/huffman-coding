f93b7a80dc7a441c1ec8b6176749bcf284d8b9f6
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-04-06 20:15:06 +0300

    feat : Create HuffmanTree class.

---
+++ src/main/java/mg/huffman/codec/HuffmanTree.java
@@ -0,0 +1,10 @@
+package mg.huffman.codec;
+
+import java.util.Map;
+
+public class HuffmanTree {
+    private Node root;
+    private Map<Character, String> encodingMap;
+    private Map<String, Character> decodingMap;
+    
+}
