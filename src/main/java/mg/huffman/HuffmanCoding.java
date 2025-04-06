6394d841bd77c1c8485a9c0bc9e0e14a801141a0
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-04-06 21:43:19 +0300

    feat : Change test into GUI.

--- src/main/java/mg/huffman/HuffmanCoding.java
+++ src/main/java/mg/huffman/HuffmanCoding.java
@@ -1,5 +1,8 @@
 package mg.huffman;
 
+import javax.swing.SwingUtilities;
+
+import mg.huffman.gui.HuffmanGUI;
 import mg.huffman.io.HuffmanIO;
 
 public class HuffmanCoding {
@@ -7,31 +10,37 @@ public class HuffmanCoding {
     /* -------------------------------------------------------------------------- */
     /*                                    Test                                    */
     /* -------------------------------------------------------------------------- */
+    // public static void main(String[] args) {
+    //     try {
+    //         if (args.length < 3) {
+    //             System.out.println("Usage: java HuffmanCoding [compress|decompress] inputFile outputFile");
+    //             return;
+    //         }
+            
+    //         String mode = args[0];
+    //         String inputFile = args[1];
+    //         String outputFile = args[2];
+            
+    //         HuffmanIO huffmanIO = new HuffmanIO();
+            
+    //         if ("compress".equals(mode)) {
+    //             huffmanIO.compressFile(inputFile, outputFile);
+    //             System.out.println("File compressed successfully!");
+    //         } else if ("decompress".equals(mode)) {
+    //             huffmanIO.decompressFile(inputFile, outputFile);
+    //             System.out.println("File decompressed successfully!");
+    //         } else {
+    //             System.out.println("Unrecognized mode. Use 'compress' or 'decompress'.");
+    //         }
+            
+    //     } catch (Exception e) {
+    //         e.printStackTrace();
+    //     }
+    // }
     public static void main(String[] args) {
-        try {
-            if (args.length < 3) {
-                System.out.println("Usage: java HuffmanCoding [compress|decompress] inputFile outputFile");
-                return;
-            }
-            
-            String mode = args[0];
-            String inputFile = args[1];
-            String outputFile = args[2];
-            
-            HuffmanIO huffmanIO = new HuffmanIO();
-            
-            if ("compress".equals(mode)) {
-                huffmanIO.compressFile(inputFile, outputFile);
-                System.out.println("File compressed successfully!");
-            } else if ("decompress".equals(mode)) {
-                huffmanIO.decompressFile(inputFile, outputFile);
-                System.out.println("File decompressed successfully!");
-            } else {
-                System.out.println("Unrecognized mode. Use 'compress' or 'decompress'.");
-            }
-            
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        SwingUtilities.invokeLater(() -> {
+            HuffmanGUI gui = new HuffmanGUI();
+            gui.setVisible(true);
+        });
     }
 }
