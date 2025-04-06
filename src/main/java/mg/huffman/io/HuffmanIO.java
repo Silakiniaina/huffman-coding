abd654783b20bb91546f803f1df35d01b15d3b5e
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-04-06 20:51:35 +0300

    feat : Add getters and setters for HuffmanIO.

--- src/main/java/mg/huffman/io/HuffmanIO.java
+++ src/main/java/mg/huffman/io/HuffmanIO.java
@@ -5,4 +5,18 @@ import mg.huffman.codec.HuffmanCodec;
 public class HuffmanIO {
     
     private HuffmanCodec codec;
+
+    /* -------------------------------------------------------------------------- */
+    /*                                   Getters                                  */
+    /* -------------------------------------------------------------------------- */
+    public HuffmanCodec getCodec() {
+        return codec;
+    }
+
+    /* -------------------------------------------------------------------------- */
+    /*                                   Setters                                  */
+    /* -------------------------------------------------------------------------- */
+    public void setCodec(HuffmanCodec codec) {
+        this.codec = codec;
+    }
 }
