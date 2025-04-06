86a071b57a6d2fa0a2cd80818014bef9333c7ad8
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-04-06 21:19:09 +0300

    feat : Add protected function showMessage, showError and showSuccess.

--- src/main/java/mg/huffman/gui/panels/BasePanel.java
+++ src/main/java/mg/huffman/gui/panels/BasePanel.java
@@ -1,6 +1,7 @@
 package mg.huffman.gui.panels;
 
 import javax.swing.BorderFactory;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 import mg.huffman.gui.ClearablePanel;
@@ -14,5 +15,19 @@ public abstract class BasePanel extends JPanel implements ClearablePanel {
        setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
     }
 
+    /* -------------------------------------------------------------------------- */
+    /*                                  Functions                                 */
+    /* -------------------------------------------------------------------------- */
+    protected void showMessage(String message, String title, int messageType) {
+        JOptionPane.showMessageDialog(this, message, title, messageType);
+    }
+
+    protected void showError(String message) {
+        showMessage(message, "Error", JOptionPane.ERROR_MESSAGE);
+    }
+
+    protected void showSuccess(String message) {
+        showMessage(message, "Success", JOptionPane.INFORMATION_MESSAGE);
+    }
     
 }
