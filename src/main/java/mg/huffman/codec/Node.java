2fe6c0bddd9425ddbbd3c11019962faefb1c5fbf
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-04-06 20:10:18 +0300

    feat : Create getters and setters for Node attributes.

--- src/main/java/mg/huffman/codec/Node.java
+++ src/main/java/mg/huffman/codec/Node.java
@@ -6,5 +6,36 @@ public class Node implements Comparable<Node> {
     Node left;
     Node right;
     boolean isLeaf;
+
+    public char getCharacter() {
+        return character;
+    }
+    public void setCharacter(char character) {
+        this.character = character;
+    }
+    public double getProbability() {
+        return probability;
+    }
+    public void setProbability(double probability) {
+        this.probability = probability;
+    }
+    public Node getLeft() {
+        return left;
+    }
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+    public Node getRight() {
+        return right;
+    }
+    public void setRight(Node right) {
+        this.right = right;
+    }
+    public boolean isLeaf() {
+        return isLeaf;
+    }
+    public void setLeaf(boolean isLeaf) {
+        this.isLeaf = isLeaf;
+    }
     
 }
