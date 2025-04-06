cf706dd4d1e30b274ebc6087a1011cc761cedfb4
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-04-06 20:19:45 +0300

    feat : Create getters and setters for HuffmanTree.

--- src/main/java/mg/huffman/codec/HuffmanTree.java
+++ src/main/java/mg/huffman/codec/HuffmanTree.java
@@ -6,5 +6,32 @@ public class HuffmanTree {
     private Node root;
     private Map<Character, String> encodingMap;
     private Map<String, Character> decodingMap;
+
+
+    /* -------------------------------------------------------------------------- */
+    /*                                   Getters                                  */
+    /* -------------------------------------------------------------------------- */
+    public Node getRoot() {
+        return root;
+    }
+    public Map<Character, String> getEncodingMap() {
+        return encodingMap;
+    }
+    public Map<String, Character> getDecodingMap() {
+        return decodingMap;
+    }
+
+    /* -------------------------------------------------------------------------- */
+    /*                                   Setter                                   */
+    /* -------------------------------------------------------------------------- */
+    public void setRoot(Node root) {
+        this.root = root;
+    }
+    public void setEncodingMap(Map<Character, String> encodingMap) {
+        this.encodingMap = encodingMap;
+    }
+    public void setDecodingMap(Map<String, Character> decodingMap) {
+        this.decodingMap = decodingMap;
+    }
     
 }
