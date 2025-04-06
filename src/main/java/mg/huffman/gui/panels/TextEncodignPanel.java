fd10562affa28092a69d868e58a98105f4f6fc1e
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-04-06 21:20:45 +0300

    feat : Create class TextEncodingPanel.

---
+++ src/main/java/mg/huffman/gui/panels/TextEncodignPanel.java
@@ -0,0 +1,16 @@
+package mg.huffman.gui.panels;
+
+import javax.swing.JButton;
+import javax.swing.JTextArea;
+
+import mg.huffman.codec.HuffmanCodec;
+
+public class TextEncodignPanel extends BasePanel{
+    
+    private final HuffmanCodec codec;
+    private final JTextArea inputArea;
+    private final JTextArea outputArea;
+    private final JTextArea mapArea;
+    private final JButton encodeButton;
+
+}
